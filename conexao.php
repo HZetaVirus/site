8ee848429356e036e93fd48433bc16add14de2da
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2022-12-27 21:13:33 -0300

    Enentedo um pouco na alterações

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$nome = 'root';
-$senha = '';
-$db = 'teste';
- 
-$conexao = new mysqli($host, $nome, $senha, $db);
-?>
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define('HOST', 'localhost');
-  define('USER', 'root');
-  define('PASS', '');
-  define('BASE', 'moob');
-
-  $conn = new mysqli_connect(HOST,USER,PASS,BASE);
-?>
